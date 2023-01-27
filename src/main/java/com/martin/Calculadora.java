beaa4305eba19f453849bd446c35120b5d195567
martin.escudero <martin.escudero@satec.es>
2023-01-27 13:51:02 +0100

    Add all my files

---
+++ src/main/java/com/martin/Calculadora.java
@@ -0,0 +1,10 @@
+package com.martin;
+
+public class Calculadora {
+
+    public Double calcular(Double a, Double b) {
+        Double d = Math.sqrt(a * a - b * b);
+
+        return b * b / 2;
+    }
+}
